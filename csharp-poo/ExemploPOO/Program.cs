ff7423181fd9c79f1ed32dd9c3a92178742d3e37
Victor Martins <vitorchiesa74@gmail.com>
2021-12-21 20:19:15 -0300

    Orientação a Objeto

---
+++ csharp-poo/ExemploPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Victor";
+            p1.Idade = 23;
+
+            p1.Apresentar();
+        }
+    }
+}

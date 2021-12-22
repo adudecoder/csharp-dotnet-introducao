15479257f91b56784b7af4417c03a4d7319e8345
Victor Martins <vitorchiesa74@gmail.com>
2021-12-22 08:07:50 -0300

    Polimorfismo

---
+++ csharp-polimorfismo/tempo de execucao/ExemploPOO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno p1 = new Aluno();
+            p1.Nome = "Victor";
+            p1.Idade = 23;
+            p1.Nota = 10;
+            p1.Apresentar();
+
+
+            Professor p2 = new Professor();
+            p2.Nome = "Gabriel";
+            p2.Idade = 35;
+            p2.Salario = 2000;
+            p2.Apresentar();
+        }
+    }
+}

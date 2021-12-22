24b0bc153d335f2cca50fe4bb43f9f13ddee6f54
Victor Martins <vitorchiesa74@gmail.com>
2021-12-21 22:10:17 -0300

    Encapsulamento

---
+++ csharp-encapsulamento/ExemploEncapsulamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ExemploEncapsulamento.Models;
+
+namespace ExemploEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Valores validos
+            Retangulo p = new Retangulo();
+            p.DefinirMedidas(30, 30);
+            
+            System.Console.WriteLine($"Área: {p.ObterArea()}");
+
+            // Valores invalidos
+            Retangulo r = new Retangulo();
+            r.DefinirMedidas(0, 0);
+            
+            System.Console.WriteLine($"Área: {r.ObterArea()}");
+        }
+    }
+}

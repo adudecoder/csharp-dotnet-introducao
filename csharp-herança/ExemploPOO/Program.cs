c96da012f33052cfc3e0cbc6f079c98c14985bec
Victor Martins <vitorchiesa74@gmail.com>
2021-12-21 23:02:16 -0300

    Herança

---
+++ csharp-herança/ExemploPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor p1 = new Professor();
+
+            p1.Salario = 2000;
+            p1.Nome = "Victor";
+            p1.Idade = 23;
+
+            p1.Apresentar();
+        }
+    }
+}

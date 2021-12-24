0c0a27a8905ca7e77acbe0a029ce5ae14120ff75
Victor Martins <vitorchiesa74@gmail.com>
2021-12-23 22:32:21 -0300

    Getters e Setters .NET

---
+++ .net-getters e setters/ExemploConstrutores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data(); // Instanciando a classe Data
+            //data.SetMes(2);
+
+            data.Mes = 12;
+            //System.Console.WriteLine(data.Mes); // Acessar propriedade
+
+            data.ApresentarMes();
+        }
+    }
+}
---
+++ .net-getters e setters/Readme.md
@@ -0,0 +1,3 @@
+# Getters e Setters
+
+O Get e Set serve para que você possa atribuir um valor em uma variável de maneira controlada e com validações.

e0d05aa8bd913ac6dea4d098d125d848c4cfd0b9
Victor Martins <vitorchiesa74@gmail.com>
2021-12-24 16:11:06 -0300

    Coleções, LINQ com .NET

---
+++ fundamentos de coleções e LINQ com .NET/.net-arrays/Colecoes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declarando um array multidimensional
+            int[,] matriz = new int[4, 2];
+            matriz[0, 0] = 5; // Na linha 0, coluna 0, coloque o valor 5
+
+            int[,] matriz2 = new int[4, 2] // Iniciando array multidimensional com valores
+            {
+                // 4 linhas
+                { 8, 8 }, // Primeira coluna e segunda coluna
+                { 10, 20 },
+                { 50, 100 },
+                { 90, 200 }
+            };
+
+            for (int i = 0; i < matriz2.GetLength(0); i++) // Percorrendo linha
+            {
+                for (int n = 0; n < matriz2.GetLength(1); n++) // Percorrendo coluna
+                {
+                    System.Console.WriteLine(matriz2[i, n]); // Imprimir os valores das linhas e colunas
+                }
+            }
+
+            // int[] arrayInteiros = new int[3]; // Array com 3 capacidades
+            // Adicionando numeros dentro do array
+            // arrayInteiros[0] = 10;
+            // arrayInteiros[1] = 20;
+            // arrayInteiros[2] = int.Parse("30"); // Converter string em number
+
+            // Index fora do array
+            // arrayInteiros[3] = 30;
+
+            // System.Console.WriteLine("Percorrendo o array pelo For");
+            // for (int i = 0; i < arrayInteiros.Length; i++) // Percorrendo o array
+            // {
+            //     System.Console.WriteLine(arrayInteiros[i]); // Imprimindo os valores do array
+            // }
+
+            // System.Console.WriteLine("Percorrendo o array pelo ForEach");
+            // foreach (int item in arrayInteiros) // Percorrendo o array
+            // {
+            //     System.Console.WriteLine(item); // Imprimindo os valores do array
+            // }
+        }
+    }
+}
---
+++ fundamentos de coleções e LINQ com .NET/.net-arrays/Readme.md
@@ -0,0 +1,37 @@
+# Arrays
+
+O array é uma estrutura de dados que armazena valores do mesmo tipo, com um tamanho fixo.
+
+* int[] array = new int[4];
+
+* int[] array = new int[] { 42, 75, 74, 61 };
+
+* string[] nomes = { "Jan", "Fev" };
+
+## Disposição do Array
+
+Índice: É a posição de um determinado valor de um array, sempre começando com zero.
+
+* var elemento = array[0]
+* array[0] = 42
+
+## Array Multidimensional
+
+O array pode possuir mais de uma dimensão:
+
+* int[,] array = new int[4, 2];
+
+4 = Número de linhas
+2 = Número de colunas
+
+Acessar um valor: array[1, 1]
+
+### Etapa dos estudos
+
+1 - Criando e Percorrendo Um Array
+
+2 - Debugando Um Array
+
+3 - Acessando Um Array De Forma Segura
+
+4 - Array Multidimensional

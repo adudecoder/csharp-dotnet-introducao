ee1984e3842603065d54b1bd7963aa0d4e5af5db
Victor Martins <vitorchiesa74@gmail.com>
2021-12-24 08:40:18 -0300

    Delegate

---
+++ .net-delegate/ExemploConstrutores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+
+        public delegate void Operacao(int x, int y);
+        static void Main(string[] args)
+        {
+            // Operacao op = Calculadora.Somar; Outra maneira de passar o metodo para o delegate sem chamar a palavra new
+            Operacao op = new Operacao(Calculadora.Somar);
+            op += Calculadora.Subtrair; // Estenção do delegate
+
+            // Ambos maneira valida de chamar o delegate
+            op.Invoke(10, 10);
+            // op(10, 10);
+        }
+    }
+}
---
+++ .net-delegate/Readme.md
@@ -0,0 +1,5 @@
+# Delegate
+Um delegate é uma maneira de passar um método como referência, podendo ser usado como um callback, aceitando qualquer método que contenha a mesma assinatura.
+
+## Multi Cast Delegate
+Estenção do Delegate, que nada mais é que você chamar varios metedos em sequencia em uma unica chamada do seu Delegate.

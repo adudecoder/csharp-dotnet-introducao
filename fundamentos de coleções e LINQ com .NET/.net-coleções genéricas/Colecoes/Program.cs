d08513dbd5e38cfded64ef1469b450d6d852b3f9
Victor Martins <vitorchiesa74@gmail.com>
2021-12-25 22:23:10 -0300

    Coleções Genéricas

---
+++ fundamentos de coleções e LINQ com .NET/.net-coleções genéricas/Colecoes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Colecoes.Helper;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OperacoesLista opLista = new OperacoesLista();
+            List<string> estados = new List<string>(); // Declarando uma List, também podemos iniciar com o valores do metodo Add List<string> estados = new List<string> { "SP", "MG", "BA" };
+            string[] estadosArray = new string[2] { "SC", "MT" };
+
+            // Adicionando elementos na List
+            estados.Add("SP");
+            estados.Add("MG");
+            estados.Add("BA");
+
+            // Add coleções na lista
+            // estados.AddRange(estadosArray);
+
+            estados.Insert(1, "RJ"); // Add elemento por indice
+
+            // System.Console.WriteLine($"Quantidade de elementos na lista: {estados.Count}"); // Retornando o numero de elementos contido na List
+
+            // Metodo de remoção de elemento
+            opLista.ImprimirListaString(estados); // Imprimir estados sem usar os For
+            // System.Console.WriteLine("Removendo o elemento");
+            // estados.Remove("MG");
+            opLista.ImprimirListaString(estados); // Ver o estado da remoção
+            // Fim do metodo de remoção de elemento
+
+            // foreach (var item in estados) // Imprimindo a lista
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+
+            // Também pode se usar o For
+            // for (int i = 0; i < estados.Count; i++) // Imprimindo a lista
+            // {
+            //     System.Console.WriteLine($"Indice {i}, valor: {estados[i]}");
+            // }
+
+        }
+    }
+}
---
+++ fundamentos de coleções e LINQ com .NET/.net-coleções genéricas/Readme.md
@@ -0,0 +1,13 @@
+# Coleções Genéricas
+No C#, existem classes de coleções que agrupam valores, e essas classes são padronizadas para as operações mais comuns, como:
+
+* Ordenação
+* Obter valor por índice
+* Obter valor com expressões
+* Trabalhar com tamanhos dinâmicos
+
+## Ordem dos estudos
+* Declarando e acessando uma lista
+* Removendo elemento da lista
+* Adicionando coleções na lista
+* Adicionando elemento por índice
